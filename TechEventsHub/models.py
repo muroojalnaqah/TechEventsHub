2c5b0960cda346d003cbaeb20508dfeceb7cf834
muroojalnaqah <muroojbaderalnaqah2004@gmail.com>
2024-08-11 12:52:49 +0300

    login,register,view,model

--- TechEventsHub/models.py
+++ TechEventsHub/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserInfo(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    fullname=models.CharField(max_length=100)
+    university=models.CharField(max_length=100)
+    degree=models.CharField(max_length=100)
+    major=models.CharField(max_length=100)
+    age=models.CharField(max_length=2)
+
+def __str__(self):
+    return f"{self.fullname}"
+
+
+
+    
--- TechEventsHub/views.py
+++ TechEventsHub/views.py
@@ -1,11 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect 
+from django.contrib.auth.models import User, auth
+from django.http import HttpResponse
+from .models import * #import all 
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 def login(request):
+    if request.method =="POST":
+        username= request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect("home")
+        else:
+            messages.info(request, "Invalid Username or Password")
+            return HttpResponseRedirect("login")
+
+
     return render(request, "login.html")
 
 def register(request):
+    if request.method =="POST":
+        username= request.POST['username']
+        fullname=request.POST['fullname']
+        password=request.POST['password']
+        age=request.POST['age']
+        university=request.POST['university']
+        degree=request.POST['degree']
+        major=request.POST['major']
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request, "Username is already taken")
+            return HttpResponseRedirect("register")
+        else:
+            user=User.objects.create_user(username=username, password=password)
+            user.save()
+            userInfo= UserInfo.objects.create(fullname=fullname, age=age, university=university, degree=degree, major=major, user_id=user.id)
+            return redirect("home")
+            
+    
+
     return render(request, "register.html")
+
+def contactus(request):
+    return render(request, "contactus.html")
+
+def user(request):
+    return render(request, "user.html")
